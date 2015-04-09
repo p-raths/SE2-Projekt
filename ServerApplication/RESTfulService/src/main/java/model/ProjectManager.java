97af974cbc53c4b91854cf8d67c15518d5bf7b47
khoepli <khoepli@cybersystems.ch>
2015-04-09 10:28:05 +0200

    Added delete-option

--- ServerApplication/RESTfulService/src/main/java/model/ProjectManager.java
+++ ServerApplication/RESTfulService/src/main/java/model/ProjectManager.java
@@ -8,12 +8,11 @@ import dataAccessObject.Project;
 import dataTransferObject.CardType;
 
 public class ProjectManager {
-	
-	
 	public ArrayList<CardType> GetFeeds()throws Exception {
 		ArrayList<CardType> feeds = null;
 		try 
 		{
+			System.out.println("GetFeed entered");
 			Database database = new Database();
 			Connection connection = database.Get_Connection();
 			Project project = new Project();
@@ -28,13 +27,13 @@ public class ProjectManager {
 	public String PostFeed(CardType cardType) throws Exception {
 		try 
 		{
-			System.out.println("PostFeed entered");
+			System.out.println("PostFeed-ProjectManager entered");
 			Database database = new Database();
 			Connection connection = database.Get_Connection();
 			Project project = new Project();
 			return project.PostFeed(cardType, connection);
 		} catch (Exception e) {
-			System.out.println("PostFeed error caused!");
+			System.out.println("PostFeed-ProjectManager error caused!");
 			throw e;
 		}
 	}
