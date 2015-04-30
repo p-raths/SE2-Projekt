e8db0aa736207ea0c204f773710ea50051856957
p-raths <p@trick-raths.ch>
2015-04-30 09:58:18 +0200

    server deleted

--- MyApplication/app/src/main/java/ch/hsr/se2/kartenverwaltung/Data/crypto.java
+++
@@ -1,7 +0,0 @@
-package ch.hsr.se2.kartenverwaltung.Data;
-
-/**
- * Created by patrick on 24.04.15.
- */
-public class crypto {
-}
--- MyApplication/app/src/main/java/ch/hsr/se2/kartenverwaltung/activities/LoginActivity.java
+++ MyApplication/app/src/main/java/ch/hsr/se2/kartenverwaltung/activities/LoginActivity.java
@@ -2,10 +2,12 @@ package ch.hsr.se2.kartenverwaltung.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.view.View;
+import android.view.View.OnClickListener;
 
+import ch.hsr.se2.kartenverwaltung.Data.Crypto;
 import ch.hsr.se2.kartenverwaltung.R;
 import roboguice.inject.ContentView;
 import roboguice.inject.InjectView;
@@ -28,6 +30,7 @@ public class LoginActivity extends CommonActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+        loginButton.setOnClickListener(new LoginListener());
 	}
 
 	public void startOverviewActivity(View view) {
@@ -35,7 +38,27 @@ public class LoginActivity extends CommonActivity {
 		startActivity(intent);
 	}
 
-	// Todo: When loginButton pressed, validate user input. Add a listener to loginButton
+    private class LoginListener implements OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+
+            // Todo: When loginButton pressed, validate user input. Add a listener to loginButton
+            Crypto crypto = new Crypto();
+
+            EditText mailForm = (EditText)findViewById(R.id.email_editText);
+            EditText passForm = (EditText)findViewById(R.id.password_editText);
+            String email = mailForm.getText().toString();
+            String password = passForm.getText().toString();
+
+            crypto.getHash(password);
+
+
+        }
+    }
+
+
+
 	// and implement it in a private class where the validator is called.
 
 }
